cbcae719fdfc6bc2cbc99e300544a1259e1fdff6
Engin Cilasun <ecilasun@me.com>
2025-10-25 01:12:59 -0700

    Revert "attempt at better AXI"
    This reverts commit 7d7851b209534b986862371b565d55a3ec75e7dd.

--- sandpiper.srcs/sources_1/new/commandqueue.sv
+++ sandpiper.srcs/sources_1/new/commandqueue.sv
@@ -1,175 +1,183 @@
-module commandqueue #(
-	parameter C_S_AXI_DATA_WIDTH = 64,
-	parameter C_S_AXI_ADDR_WIDTH = 32
-) (
-	// Global signals
-	input wire  aclk,
-	input wire  aresetn,
+`timescale 1ns / 1ps
 
-	// Write address channel
-	input wire  m_axi_awvalid,
-	output wire m_axi_awready,
-	input wire  [C_S_AXI_ADDR_WIDTH-1:0] m_axi_awaddr,
-
-	// Write data channel
-	input wire  m_axi_wvalid,
-	output wire m_axi_wready,
-	input wire  [C_S_AXI_DATA_WIDTH-1:0] m_axi_wdata,
-	input wire  [(C_S_AXI_DATA_WIDTH/8)-1:0] m_axi_wstrb,
-
-	// Write response channel
-	output wire m_axi_bvalid,
-	input wire  m_axi_bready,
-	output wire [1:0] m_axi_bresp,
-
-	// Read address channel
-	input wire  m_axi_arvalid,
-	output wire m_axi_arready,
-	input wire  [C_S_AXI_ADDR_WIDTH-1:0] m_axi_araddr,
-
-	// Read data channel
-	output wire m_axi_rvalid,
-	input wire  m_axi_rready,
-	output wire [C_S_AXI_DATA_WIDTH-1:0] m_axi_rdata,
-	output wire [1:0] m_axi_rresp,
-
-	// Fifo interface
+module commandqueue(
+	input wire aclk,
+	input wire aresetn,
+	output logic m_axi_arready,
+	output logic m_axi_awready,
+	output logic m_axi_bvalid,
+	output logic m_axi_rlast,
+	output logic m_axi_rvalid,
+	output logic m_axi_wready,
+	output logic [1:0] m_axi_bresp,
+	output logic [1:0] m_axi_rresp,
+	output logic [5:0] m_axi_bid,
+	output logic [5:0] m_axi_rid,
+	output logic [63:0] m_axi_rdata,
+	input wire m_axi_arvalid,
+	input wire m_axi_awvalid,
+	input wire m_axi_bready,
+	input wire m_axi_rready,
+	input wire m_axi_wlast,
+	input wire m_axi_wvalid,
+	input wire [1:0] m_axi_arburst,
+	input wire [1:0] m_axi_arlock,
+	input wire [2:0] m_axi_arsize,
+	input wire [1:0] m_axi_awburst,
+	input wire [1:0] m_axi_awlock,
+	input wire [2:0] m_axi_awsize,
+	input wire [2:0] m_axi_arprot,
+	input wire [2:0] m_axi_awprot,
+	input wire [31:0] m_axi_araddr,
+	input wire [31:0] m_axi_awaddr,
+	input wire [3:0] m_axi_arcache,
+	input wire [3:0] m_axi_arlen,
+	input wire [3:0] m_axi_arqos,
+	input wire [3:0] m_axi_awcache,
+	input wire [3:0] m_axi_awlen,
+	input wire [3:0] m_axi_awqos,
+	input wire [5:0] m_axi_arid,
+	input wire [5:0] m_axi_awid,
+	input wire [5:0] m_axi_wid,
+	input wire [63:0] m_axi_wdata,
+	input wire [7:0] m_axi_wstrb,
+	// Internal comms channel for device
 	output wire fifoempty,
 	output wire [31:0] fifodout,
 	input wire fifore,
 	output wire fifovalid,
+	// Device state such as bit flags / counters
+    input wire [31:0] devicestateLow,
+    // Upper device state
+    input wire [31:0] devicestateHigh);
 
-	// Low/High device state words (status bit flags / counters etc)
-	input wire [31:0] devicestateLow,
-	input wire [31:0] devicestateHigh);
+assign m_axi_bresp = 2'b00;
+assign m_axi_rresp = 2'b00;
+assign m_axi_bid = 6'd0;
+assign m_axi_rid = 6'd0;
 
-	// --- State machine and internal registers ---
-	reg awready_reg;
-	reg wready_reg;
-	reg bvalid_reg;
-	reg [1:0] bresp_reg;
-	reg arready_reg;
-	reg rvalid_reg;
-	reg [C_S_AXI_DATA_WIDTH-1:0] rdata_reg;
-	reg [1:0] rresp_reg;
+// --------------------------------------------------
+// Device command FIFO
+// --------------------------------------------------
 
-	reg [C_S_AXI_ADDR_WIDTH-1:0] awaddr_reg;
-	reg [C_S_AXI_ADDR_WIDTH-1:0] araddr_reg;
+wire fifofull;
+logic fifowe;
+logic [31:0] fifodin;
 
-	// AXI4-Lite handshake logic
-	assign m_axi_awready = awready_reg;
-	assign m_axi_wready = wready_reg;
-	assign m_axi_bvalid = bvalid_reg;
-	assign m_axi_bresp = bresp_reg;
-	assign m_axi_arready = arready_reg;
-	assign m_axi_rvalid = rvalid_reg;
-	assign m_axi_rdata = rdata_reg;
-	assign m_axi_rresp = rresp_reg;
+cmdfifo cmdfifoinst(
+	.full(fifofull),
+	.din(fifodin),
+	.wr_en(fifowe),
+	.empty(fifoempty),
+	.dout(fifodout),
+	.rd_en(fifore),
+	.valid(fifovalid),
+	.clk(aclk),
+	.rst(~aresetn) );
 
-	wire fifofull;
-	logic fifowe;
-	logic [31:0] fifodin;
+logic [1:0] waddrstate;
+logic [1:0] writestate;
+logic [1:0] raddrstate;
 
-	cmdfifo cmdfifoinst(
-		.full(fifofull),
-		.din(fifodin),
-		.wr_en(fifowe),
-		.empty(fifoempty),
-		.dout(fifodout),
-		.rd_en(fifore),
-		.valid(fifovalid),
-		.clk(aclk),
-		.rst(~aresetn) );
-
-	// --- Write state machine ---
-	localparam [1:0] WRITE_IDLE = 2'b00;
-	localparam [1:0] WRITE_ADDR = 2'b01;
-	localparam [1:0] WRITE_DATA = 2'b10;
-	localparam [1:0] WRITE_RESP = 2'b11;
-	reg [1:0] write_state;
-
-	always @(posedge aclk or negedge aresetn) begin
-		if (!aresetn) begin
-			// Reset state
-			awready_reg <= 1'b0;
-			wready_reg <= 1'b0;
-			bvalid_reg <= 1'b0;
-			bresp_reg <= 2'b0;
-			write_state <= WRITE_IDLE;
-			fifowe <= 1'b0;
-			fifodin <= 32'd0;
-		end else begin
-			fifowe <= 1'b0;
-			// Write channel logic
-			case (write_state)
-				WRITE_IDLE: begin
-					awready_reg <= 1'b1;
-					if (m_axi_awvalid) begin
-						awaddr_reg <= m_axi_awaddr;
-						awready_reg <= 1'b0;
-						write_state <= WRITE_DATA;
-					end
+always @(posedge aclk) begin
+	if (~aresetn) begin
+		waddrstate <= 2'b00;
+	end else begin
+		unique case (waddrstate)
+			2'b00: begin
+				m_axi_awready <= 1'b0;
+				waddrstate <= 2'b01;
+			end
+			2'b01: begin
+				if (m_axi_awvalid) begin
+					m_axi_awready <= 1'b1;
+					waddrstate <= 2'b10;
 				end
-				WRITE_DATA: begin
-					wready_reg <= 1'b1;
-					if (m_axi_wvalid && ~fifofull) begin
-						fifowe <= 1'b1;
-						fifodin <= m_axi_wdata[31:0]; // Assuming we're always allowed to do 32-bit writes
-						wready_reg <= 1'b0;
-						write_state <= WRITE_RESP;
-					end
-				end
-				WRITE_RESP: begin
-					bvalid_reg <= 1'b1;
-					bresp_reg <= 2'b00; // OKAY response
-					if (m_axi_bready) begin
-						bvalid_reg <= 1'b0;
-						write_state <= WRITE_IDLE;
-					end
-				end
-				default: write_state <= WRITE_IDLE;
-			endcase
-		end
+			end
+			2'b10: begin
+				m_axi_awready <= 1'b0;
+				waddrstate <= 2'b01;
+			end
+		endcase
 	end
+end
 
-	// --- Read state machine ---
-	localparam [1:0] READ_IDLE = 2'b00;
-	localparam [1:0] READ_ADDR = 2'b01;
-	localparam [1:0] READ_DATA = 2'b10;
-	reg [1:0] read_state;
-
-	always @(posedge aclk or negedge aresetn) begin
-		if (!aresetn) begin
-			// Reset state
-			arready_reg <= 1'b0;
-			rvalid_reg <= 1'b0;
-			rdata_reg <= 0;
-			rresp_reg <= 2'b0;
-			read_state <= READ_IDLE;
-		end else begin
-			case (read_state)
-				READ_IDLE: begin
-					arready_reg <= 1'b1;
-					if (m_axi_arvalid) begin
-						araddr_reg <= m_axi_araddr;
-						arready_reg <= 1'b0;
-						read_state <= READ_DATA;
-					end
+always @(posedge aclk) begin
+	if (~aresetn) begin
+		fifowe <= 1'b0;
+		writestate <= 2'b00;
+		fifodin <= 32'd0;
+	end else begin
+		fifowe <= 1'b0;
+		m_axi_wready <= 1'b0;
+		m_axi_bvalid <= 1'b0;
+	
+		unique case (writestate)
+			2'b00: begin
+				m_axi_bvalid <= 1'b0;
+				m_axi_wready <= 1'b0;
+				writestate <= 2'b01;
+			end
+			2'b01: begin
+				if (m_axi_wvalid && ~fifofull) begin
+					fifodin <= m_axi_wdata[31:0];
+					fifowe <= 1'b1;
+					writestate <= 2'b10;
+					m_axi_wready <= 1'b1;
 				end
-				READ_DATA: begin
-					unique case (araddr_reg[2])
-						1'b0: rdata_reg <= {devicestateLow, devicestateLow};
-						1'b1: rdata_reg <= {devicestateHigh, devicestateHigh};
+			end
+			2'b10: begin
+				if (m_axi_bready) begin
+					m_axi_bvalid <= 1'b1;
+					writestate <= 2'b01;
+				end
+			end
+		endcase
+	end
+end
+
+always @(posedge aclk) begin
+	if (~aresetn) begin
+		raddrstate <= 2'b00;
+		m_axi_rlast <= 1'b0;
+	end else begin
+		m_axi_rvalid <= 1'b0;
+		m_axi_arready <= 1'b0;
+		m_axi_rlast <= 1'b0;
+
+		unique case (raddrstate)
+			2'b00: begin
+				m_axi_arready <= 1'b0;
+				m_axi_rvalid <= 1'b0;
+				raddrstate <= 2'b01;
+			end
+			2'b01: begin
+				if (m_axi_arvalid) begin
+					m_axi_arready <= 1'b1;
+					case (m_axi_araddr[3:0])
+						4'b0000: raddrstate <=  2'b10; // =0x_0 low device state
+						default: raddrstate <=  2'b11; // !0x_0 high device state
 					endcase
-					rvalid_reg <= 1'b1;
-					rresp_reg <= 2'b00; // OKAY response
-					if (m_axi_rready) begin
-						rvalid_reg <= 1'b0;
-						read_state <= READ_IDLE;
-					end
 				end
-				default: read_state <= READ_IDLE;
-			endcase
-		end
+			end
+			2'b10: begin
+				if (m_axi_rready) begin
+					m_axi_rdata <= {devicestateLow, devicestateLow};
+					m_axi_rvalid <= 1'b1;
+					m_axi_rlast <= 1'b1;
+					raddrstate <= 2'b01;
+				end
+			end
+			2'b11: begin
+				if (m_axi_rready) begin
+					m_axi_rdata <= {devicestateHigh, devicestateHigh};
+					m_axi_rvalid <= 1'b1;
+					m_axi_rlast <= 1'b1;
+					raddrstate <= 2'b01;
+				end
+			end
+		endcase
 	end
+end
+
 endmodule
